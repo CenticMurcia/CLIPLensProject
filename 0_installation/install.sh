224b2f868a883303c516135ed28f3434d047f182
Raquel <43879685+reshiTrent@users.noreply.github.com>
2024-03-16 13:03:30 +0100

    final commit

---
+++ 0_installation/install.sh
@@ -0,0 +1,4 @@
+pip install --user accelerate
+pip install --user transformers==4.25.1
+pip install --user diffusers==0.11.0 
+pip install --user git+https://github.com/cloneofsimo/lora.git
---
+++ 2_train_scripts/train_silla2_SD21.sh
@@ -0,0 +1,33 @@
+export INSTANCE_DIR="../1_imgs_in/silla2"
+export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
+export OUTPUT_DIR="../3_generated_models/silla2-SD21"
+
+lora_pti \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --train_text_encoder \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --scale_lr \
+  --learning_rate_unet=1e-5 \
+  --learning_rate_text=1e-5 \
+  --learning_rate_ti=5e-4 \
+  --color_jitter \
+  --lr_scheduler="linear" \
+  --lr_warmup_steps=0 \
+  --placeholder_tokens="<silla2>" \
+  --initializer_tokens="chair" \
+  --use_template="style"\
+  --save_steps=500 \
+  --max_train_steps_ti=500 \
+  --max_train_steps_tuning=1000 \
+  --perform_inversion=True \
+  --clip_ti_decay \
+  --weight_decay_ti=0.000 \
+  --weight_decay_lora=0.001\
+  --continue_inversion \
+  --continue_inversion_lr=1e-4 \
+  --device="cuda:0" \
+  --lora_rank=1
